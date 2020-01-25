9a89b3704b4a99c080fc76a4e0be88405ae2dc6a
John Lavelle <john@jql.co.uk>
2020-01-25 18:09:15 +0100

    Comment added

--- src/Calculator.php
+++ src/Calculator.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,58 +17,67 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ * Tutorial
  */
 
 /**
  * Simple class for our unit tests.
  */
-class Calculator {
+class Calculator
+{
 
-    /**
-     * @assert (0, 0) == 0
-     * @assert (0, 1) == 1
-     * @assert (1, 0) == 1
-     * @assert (1, 1) == 2
-     */
-    public function plus($a, $b) {
-        return $a + $b;
-    }
+  /**
+   * @assert (0, 0) == 0
+   * @assert (0, 1) == 1
+   * @assert (1, 0) == 1
+   * @assert (1, 1) == 2
+   */
+  public function plus($a, $b)
+  {
+    return $a + $b;
+  }
 
-    /**
-     * @assert (0, 0) == 0
-     * @assert (0, 1) == -1
-     * @assert (1, 0) == 1
-     * @assert (1, 1) == 0
-     */
-    public function minus($a, $b) {
-        return $a - $b;
-    }
+  /**
+   * @assert (0, 0) == 0
+   * @assert (0, 1) == -1
+   * @assert (1, 0) == 1
+   * @assert (1, 1) == 0
+   */
+  public function minus($a, $b)
+  {
+    return $a - $b;
+  }
 
-    /**
-     * @assert (0, 0) == 0
-     * @assert (0, 1) == 0
-     * @assert (1, 0) == 0
-     * @assert (1, 1) == 1
-     * @assert (3, 2) == 6
-     */
-    public function multiply($a, $b) {
-        return $a * $b;
-    }
+  /**
+   * @assert (0, 0) == 0
+   * @assert (0, 1) == 0
+   * @assert (1, 0) == 0
+   * @assert (1, 1) == 1
+   * @assert (3, 2) == 6
+   */
+  public function multiply($a, $b)
+  {
+    return $a * $b;
+  }
 
-    /**
-     * @assert (0, 1) == 0
-     * @assert (1, 1) == 1
-     * @assert (6, 2) == 3
-     */
-    public function divide($a, $b) {
-        if ($b == 0) {
-            throw new InvalidArgumentException('Cannot divide by zero');
-        }
-        return $a / $b;
+  /**
+   * @assert (0, 1) == 0
+   * @assert (1, 1) == 1
+   * @assert (6, 2) == 3
+   */
+  public function divide($a, $b)
+  {
+    if ($b == 0)
+    {
+      throw new InvalidArgumentException('Cannot divide by zero');
     }
+    return $a / $b;
+  }
 
-    public function modulo($a, $b) {
-        return $a % $b;
-    }
+  public function modulo($a, $b)
+  {
+    return $a % $b;
+  }
 
 }
